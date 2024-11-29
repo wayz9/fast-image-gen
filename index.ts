00eec4abe2944ee48fa487eebab45bee9909ba2a
Vukasin Vitorovic <ventusblade1@gmail.com>
2024-11-29 13:50:04 +0100

    revert

--- index.ts
+++ index.ts
@@ -8,14 +8,14 @@ const browser = await puppeteer.launch({
   args: ["--no-sandbox"],
 });
 
+const page = await browser.newPage();
+
 app.post("/", async (context) => {
   const { path, html, file_name } = (await context.req.json()) as {
     path: string;
     html: string;
     file_name: string;
   };
-
-  const page = await browser.newPage();
 
   await page.setContent(html);
   const product = await page.waitForSelector("div#product");
@@ -24,8 +24,6 @@ app.post("/", async (context) => {
     path: `${path}/${file_name}`,
     omitBackground: true,
   });
-
-  await page.close();
 
   return context.json({ success: true });
 });
